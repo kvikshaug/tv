e102cb557978181fb367301d8b55ea6f9d5db686
Ali Kaafarani <ali@kvikshaug.no>
2018-10-05 23:05:29 +0200

    chore: setuptools

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='tv',
+    version='0.0.1',
+    py_modules=['tv'],
+    install_requires=[
+        'click',
+        'requests',
+        'tabulate',
+    ],
+    entry_points="""
+        [console_scripts]
+        tv=tv.tv:main
+    """,
+)
